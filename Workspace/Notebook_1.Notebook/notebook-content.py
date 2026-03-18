2bfffc256f09b909bcad92a42ba4c2007656e791
Bob Duffy <bob@prodata.ie>
2026-03-18 19:21:32 +0000

    Committing 1 item from workspace 1de6b5c2-d489-4810-8489-5a08ce14d8bb

---
+++ Workspace/Notebook_1.Notebook/notebook-content.py
@@ -0,0 +1,23 @@
+# Fabric notebook source
+
+# METADATA ********************
+
+# META {
+# META   "kernel_info": {
+# META     "name": "synapse_pyspark"
+# META   },
+# META   "dependencies": {}
+# META }
+
+# CELL ********************
+
+# Welcome to your new notebook
+# Type here in the cell editor to add code!
+
+
+# METADATA ********************
+
+# META {
+# META   "language": "python",
+# META   "language_group": "synapse_pyspark"
+# META }
